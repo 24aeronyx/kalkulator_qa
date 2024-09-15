6b337742b68aa9122db03d16744b66bb7eec3479
AmandaSabina <07211009@student.itk.ac.id>
2024-09-15 22:51:06 +0800

    Update styling

--- src/pages/RuRumus.jsx
+++ src/pages/RuRumus.jsx
@@ -7,7 +7,7 @@ export default function Rurumus() {
         <div className="relative">
             <Link
                 to="/ru"
-                className="absolute top-14 left-1 bg-white border-2 border-black rounded-full p-2 shadow-md duration-300 lg:top-10 lg:left-4 hover:bg-gray-200"
+                className="absolute top-4 left-1 bg-white border-2 border-black rounded-full p-2 shadow-md duration-300 lg:top-10 lg:left-4 hover:bg-gray-200"
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
